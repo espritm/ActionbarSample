77a4ea82b9e18a982cbee644f0ad1344e2d8d5b3
Maxime Esprit <maxime.esprit@gmail.com>
2017-03-22 12:05:58 +0100

    Création

---
+++ ActionbarSample/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace App1
+{
+    [Activity(Label = "@string/titleBase", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+
+        public override bool OnCreateOptionsMenu(Android.Views.IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.cloche:
+                    if (SupportActionBar.Title == Resources.GetString(Resource.String.titleRenamed))
+                        SupportActionBar.Title = Resources.GetString(Resource.String.titleBase);
+                    else
+                        SupportActionBar.Title = Resources.GetString(Resource.String.titleRenamed);
+                    break;
+
+                case Resource.Id.moreoption:
+                    //...
+                    break;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
